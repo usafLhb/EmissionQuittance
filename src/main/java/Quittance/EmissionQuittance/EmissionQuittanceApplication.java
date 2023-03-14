a6a499fff2777741471988e6cb5fd0dd0eb3238e
lahbabta youssef <y.lahbabta@rmaassurance.com>
2023-03-14 09:22:39 +0100

    Initial commit

---
+++ src/main/java/Quittance/EmissionQuittance/EmissionQuittanceApplication.java
@@ -0,0 +1,13 @@
+package Quittance.EmissionQuittance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissionQuittanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissionQuittanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Quittance/EmissionQuittance/EmissionQuittanceApplicationTests.java
@@ -0,0 +1,13 @@
+package Quittance.EmissionQuittance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissionQuittanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
