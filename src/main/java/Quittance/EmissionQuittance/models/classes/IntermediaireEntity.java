3824fe832345c9a409308f7570760dfb5dcbd193
R0001324 <youssef.lahbabta@gmail.com>
2023-03-30 14:16:29 +0200

    add entities

---
+++ src/main/java/Quittance/EmissionQuittance/models/classes/IntermediaireEntity.java
@@ -0,0 +1,38 @@
+package Quittance.EmissionQuittance.models.classes;
+
+import Quittance.EmissionQuittance.models.Police;
+import Quittance.EmissionQuittance.models.Quittance;
+import Quittance.EmissionQuittance.models.Ref_Commerciale;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Intermediaire", schema = "CONVERGPARAM", catalog = "")
+public class IntermediaireEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ref_intermediaire")
+    private Ref_Commerciale ref_Commerciale;
+    @Column(name = "NOMIntermediaire")
+    private String nomCommercial ;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Quittance_id")
+    private QtcQuittanceEntity quittance;
+
+    @OneToMany(mappedBy = "prd_versionCommerciale")
+    private List<Police> polices;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ref_Commerciale_id")
+    private Ref_Commerciale refCommerciale;
+}
