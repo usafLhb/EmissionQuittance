5d8cef94ec0d25725d7a6e4028fd94044ba7a39b
usafLhb <youssef.lahbabta@gmail.com>
2023-05-11 21:23:32 +0100

    ...

--- src/main/java/Quittance/EmissionQuittance/security/user/UserRepository.java
+++ src/main/java/Quittance/EmissionQuittance/security/user/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+
     Optional<User> findByEmail(String email);
+
 }
